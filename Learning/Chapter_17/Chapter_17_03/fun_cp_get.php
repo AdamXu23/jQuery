f0858b058698b2c0eff1af8ed5acae5e35df6c46
AdamXu <xsz1993@live.com>
2022-09-19 16:42:26 +0800

    123

--- Learning/Chapter_17/Chapter_17_03/fun_cp_get.php
+++ Learning/Chapter_17/Chapter_17_03/fun_cp_get.php
@@ -1,8 +1,16 @@
-<?php
-    if(isset($_GET['send'])&&($_GET['send']!="")){
-        echo "傳送方式為：".$_GET['send'].",值為：".$_GET['sendval'];
-    }
-    if (isset($_POST['send']) && ($_POST['send'] != "")) {
-        echo "傳送方式為：" . $_POST['send'] . ",值為：" . $_POST['sendval'];
-    }
-?>
+<!DOCTYPE html>
+<html lang="zh">
+    <body>
+        <div data-role="content" style="text-align:center;">
+            <?php
+                if(isset($_GET['send'])&&($_GET['send']!="")){
+                    echo "傳送方式為：".$_GET['send'].",值為：".$_GET['sendval'];
+                }
+
+                if (isset($_POST['send']) && ($_POST['send'] != "")) {
+                    echo "傳送方式為：" . $_POST['send'] . ",值為：" . $_POST['sendval'];
+                }
+            ?>
+        </div> 
+    </body>
+</html>
